049b3122a11b27ff8272d382ee0a2ac3a82023af
Patbox <39821509+Patbox@users.noreply.github.com>
2024-06-12 09:01:56 +0200

    Supply wrapper lookup

--- src/main/java/eu/pb4/predicate/api/GsonPredicateSerializer.java
+++ src/main/java/eu/pb4/predicate/api/GsonPredicateSerializer.java
@@ -2,19 +2,29 @@ package eu.pb4.predicate.api;
 
 import com.google.gson.*;
 import com.mojang.serialization.JsonOps;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryWrapper;
 
 import java.lang.reflect.Type;
 
 public final class GsonPredicateSerializer implements JsonSerializer<MinecraftPredicate>, JsonDeserializer<MinecraftPredicate> {
-    public static final GsonPredicateSerializer INSTANCE = new GsonPredicateSerializer();
+    @Deprecated(forRemoval = true)
+    public static final GsonPredicateSerializer INSTANCE = create(DynamicRegistryManager.of(Registries.REGISTRIES));
+    private final RegistryWrapper.WrapperLookup lookup;
 
+    public static GsonPredicateSerializer create(RegistryWrapper.WrapperLookup lookup) {
+        return new GsonPredicateSerializer(lookup);
+    }
 
-    private GsonPredicateSerializer() {}
+    private GsonPredicateSerializer(RegistryWrapper.WrapperLookup lookup) {
+        this.lookup = lookup;
+    }
 
     @Override
     public MinecraftPredicate deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         try {
-            return PredicateRegistry.decode(jsonElement);
+            return PredicateRegistry.decode(this.lookup, jsonElement);
         } catch (Throwable t) {
             throw new JsonParseException(t);
         }
@@ -22,6 +32,6 @@ public final class GsonPredicateSerializer implements JsonSerializer<MinecraftPr
 
     @Override
     public JsonElement serialize(MinecraftPredicate minecraftPredicate, Type type, JsonSerializationContext jsonSerializationContext) {
-        return PredicateRegistry.CODEC.encode(minecraftPredicate, JsonOps.INSTANCE, new JsonObject()).result().get();
+        return PredicateRegistry.CODEC.encode(minecraftPredicate, this.lookup.getOps(JsonOps.INSTANCE), new JsonObject()).result().get();
     }
 }
--- src/main/java/eu/pb4/predicate/api/PredicateRegistry.java
+++ src/main/java/eu/pb4/predicate/api/PredicateRegistry.java
@@ -7,6 +7,10 @@ import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.MapCodec;
 import eu.pb4.predicate.impl.BaseCodec;
 import eu.pb4.predicate.impl.PredicatesInit;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryOps;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,15 +20,17 @@ import java.util.Map;
 public final class PredicateRegistry {
     private static final Map<Identifier, MapCodec<MinecraftPredicate>> CODECS = new HashMap<>();
     private static final Map<MapCodec<MinecraftPredicate>, Identifier> CODEC_IDS = new HashMap<>();
-    public static final Codec<MinecraftPredicate> CODEC = new MapCodec.MapCodecCodec<>(new BaseCodec());
+    private static final RegistryWrapper.WrapperLookup FALLBACK_LOOKUP = DynamicRegistryManager.of(Registries.REGISTRIES);
+    public static final MapCodec<MinecraftPredicate> MAP_CODEC = new BaseCodec();
+    public static final Codec<MinecraftPredicate> CODEC = new MapCodec.MapCodecCodec<>(MAP_CODEC);
 
     private PredicateRegistry() {}
 
-    public static MinecraftPredicate decode(JsonElement object) {
-        return decode(JsonOps.INSTANCE, object);
+    public static MinecraftPredicate decode(RegistryWrapper.WrapperLookup lookup, JsonElement object) {
+        return decode(lookup.getOps(JsonOps.INSTANCE), object);
     }
 
-    public static <T> MinecraftPredicate decode(DynamicOps<T> ops, T object) {
+    public static <T> MinecraftPredicate decode(RegistryOps<T> ops, T object) {
         var data = CODEC.decode(ops, object);
         return data.getOrThrow(IllegalArgumentException::new).getFirst();
     }
@@ -42,6 +48,19 @@ public final class PredicateRegistry {
     public static <T extends MinecraftPredicate> void register(Identifier identifier, MapCodec<T> predicateCodec) {
         CODECS.put(identifier, (MapCodec<MinecraftPredicate>) predicateCodec);
         CODEC_IDS.put((MapCodec<MinecraftPredicate>) predicateCodec, identifier);
+    }
+
+    @Deprecated(forRemoval = true)
+    public static MinecraftPredicate decode(JsonElement object) {
+        return decode(JsonOps.INSTANCE, object);
+    }
+    @Deprecated(forRemoval = true)
+    public static <T> MinecraftPredicate decode(DynamicOps<T> ops, T object) {
+        if (ops instanceof RegistryOps<T> registryOps) {
+            return decode(registryOps, object);
+        }
+
+        return decode(FALLBACK_LOOKUP.getOps(ops), object);
     }
 
     static {

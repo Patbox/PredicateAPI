9a6b6a59eae7e99f248a8d8162b3bfecdd1557d7
Patbox <39821509+Patbox@users.noreply.github.com>
2025-09-20 09:25:36 +0200

    Update to 1.21.9

--- src/main/java/eu/pb4/predicate/api/PredicateContext.java
+++ src/main/java/eu/pb4/predicate/api/PredicateContext.java
@@ -3,6 +3,7 @@ package eu.pb4.predicate.api;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerConfigEntry;
 import net.minecraft.server.command.CommandOutput;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -40,12 +41,12 @@ public record PredicateContext(MinecraftServer server,
     }
 
     public static PredicateContext of(GameProfile profile, MinecraftServer server) {
-        var name = profile.getName() != null ? profile.getName() : profile.getId().toString();
-        return new PredicateContext(server, new ServerCommandSource(CommandOutput.DUMMY, Vec3d.ZERO, Vec2f.ZERO, server.getOverworld(), server.getPermissionLevel(profile), name, Text.literal(name), server, null), null, null, null, profile);
+        var name = !profile.name().isEmpty() ? profile.name() : profile.id().toString();
+        return new PredicateContext(server, new ServerCommandSource(CommandOutput.DUMMY, Vec3d.ZERO, Vec2f.ZERO, server.getOverworld(), server.getPermissionLevel(new PlayerConfigEntry(profile)), name, Text.literal(name), server, null), null, null, null, profile);
     }
 
     public static PredicateContext of(ServerPlayerEntity player) {
-        return new PredicateContext(player.getServer(), player.getCommandSource(), player.getServerWorld(), player, player, player.getGameProfile());
+        return new PredicateContext(player.getEntityWorld().getServer(), player.getCommandSource(), player.getEntityWorld(), player, player, player.getGameProfile());
     }
 
     public static PredicateContext of(ServerCommandSource source) {
@@ -56,7 +57,7 @@ public record PredicateContext(MinecraftServer server,
         if (entity instanceof ServerPlayerEntity player) {
             return of(player);
         } else {
-            return new PredicateContext(entity.getServer(), entity.getCommandSource((ServerWorld) entity.getWorld()), (ServerWorld) entity.getWorld(), null, entity, null);
+            return new PredicateContext(entity.getEntityWorld().getServer(), entity.getCommandSource((ServerWorld) entity.getEntityWorld()), (ServerWorld) entity.getEntityWorld(), null, entity, null);
         }
     }
 }

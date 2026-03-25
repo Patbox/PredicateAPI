4f63f8df3bd5ec2331ab328ebd69c343f187fa01
Patbox <patbox4@outlook.com>
2026-03-25 09:35:25 +0100

    Readd support for legacy permission api

--- src/main/java/eu/pb4/predicate/impl/FabricPermissionBridge.java
+++ src/main/java/eu/pb4/predicate/impl/FabricPermissionBridge.java
@@ -1,5 +1,6 @@
 package eu.pb4.predicate.impl;
 
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.resources.Identifier;
@@ -10,23 +11,22 @@ import net.minecraft.world.entity.player.Player;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.util.function.Predicate;
-
-
 /**
  * Temporary wrapper for permission checks, targets yet to be merged fabric-permission-api-v1, making the mod support it before it's finalized.
  * Also contains a fallback when it's not present or it changes and fails to adapt.
  */
 public class FabricPermissionBridge {
     public static final boolean IS_LOADED = FabricLoader.getInstance().isModLoaded("fabric-permission-api-v1");
+    public static final boolean IS_LOADED_LEGACY = FabricLoader.getInstance().isModLoaded("fabric-permissions-api-v0");
 
     private static MethodHandle permissionCheckCallPlayerLevel;
     private static MethodHandle permissionCheckCallCommandLevel;
     private static MethodHandle permissionCheckCallPlayerBool;
     private static MethodHandle permissionCheckCallCommandBool;
-    private static final boolean enabled;
+    private static final boolean enabledFabric;
 
     public static boolean checkPermission(Player player, Identifier permission, PermissionLevel level) {
-        if (enabled) {
+        if (enabledFabric) {
             try {
                 return (boolean) permissionCheckCallPlayerLevel.bindTo(player).invokeExact(permission, level);
             } catch (Throwable e) {
@@ -35,11 +35,15 @@ public class FabricPermissionBridge {
             }
         }
 
+        if (IS_LOADED_LEGACY) {
+            return Permissions.check(player, permission.toShortLanguageKey().replace('/', '.'), level);
+        }
+
         return player.permissions().hasPermission(new Permission.HasCommandLevel(level));
     }
 
     public static boolean checkPermission(CommandSourceStack player, Identifier permission, PermissionLevel level) {
-        if (enabled) {
+        if (enabledFabric) {
             try {
                 return (boolean) permissionCheckCallCommandLevel.bindTo(player).invokeExact(permission, level);
             } catch (Throwable e) {
@@ -48,11 +52,15 @@ public class FabricPermissionBridge {
             }
         }
 
+        if (IS_LOADED_LEGACY) {
+            return Permissions.check(player, permission.toShortLanguageKey().replace('/', '.'), level);
+        }
+
         return player.permissions().hasPermission(new Permission.HasCommandLevel(level));
     }
 
     public static boolean checkPermission(Player player, Identifier permission, boolean defaultValue) {
-        if (enabled) {
+        if (enabledFabric) {
             try {
                 return (boolean) permissionCheckCallPlayerBool.bindTo(player).invokeExact(permission, defaultValue);
             } catch (Throwable e) {
@@ -61,11 +69,15 @@ public class FabricPermissionBridge {
             }
         }
 
+        if (IS_LOADED_LEGACY) {
+            return Permissions.check(player, permission.toShortLanguageKey().replace('/', '.'), defaultValue);
+        }
+
         return defaultValue;
     }
 
     public static boolean checkPermission(CommandSourceStack player, Identifier permission, boolean defaultValue) {
-        if (enabled) {
+        if (enabledFabric) {
             try {
                 return (boolean) permissionCheckCallCommandBool.bindTo(player).invokeExact(permission, defaultValue);
             } catch (Throwable e) {
@@ -74,11 +86,19 @@ public class FabricPermissionBridge {
             }
         }
 
+        if (IS_LOADED_LEGACY) {
+            return Permissions.check(player, permission.toShortLanguageKey().replace('/', '.'), defaultValue);
+        }
+
         return defaultValue;
     }
 
     public static Predicate<CommandSourceStack> require(Identifier permission, PermissionLevel level) {
         return ctx -> checkPermission(ctx, permission, level);
+    }
+
+    public static Predicate<CommandSourceStack> require(Identifier permission, boolean def) {
+        return ctx -> checkPermission(ctx, permission, def);
     }
 
     private static MethodHandle findCheckPermission(Class<?> object, Class<?> fallback) throws Throwable {
@@ -100,6 +120,6 @@ public class FabricPermissionBridge {
                 err.printStackTrace();
             }
         }
-        enabled = e;
+        enabledFabric = e;
     }
 }
